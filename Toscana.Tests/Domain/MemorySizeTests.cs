8a1c8e31e94a9c043cf3c1e42c507bd44fdbc064
Boris Modylevsky <borismod@gmail.com>
2016-06-02 20:10:46 +0300

    MemorySize class was added

---
+++ Toscana.Tests/Domain/MemorySizeTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Toscana.Domain;
+
+namespace Toscana.Tests.Domain
+{
+    [TestFixture]
+    public class MemorySizeTests
+    {
+        [Test]
+        public void MemorySize_10MB_ParsedToTotalBytes()
+        {
+            var memorySize = new MemorySize("10 MB");
+
+            memorySize.TotalBytes.Should().Be(10485760);
+        }
+
+        [Test]
+        public void MemorySize_IsBinarySerializable()
+        {
+            var memorySize = new MemorySize("10 MB");
+
+            memorySize.Should().BeBinarySerializable();
+        }
+    }
+}
---
+++ Toscana/Domain/MemorySize.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace Toscana.Domain
+{
+    [Serializable]
+    public struct MemorySize : ISerializable
+    {
+        private const string TotaBytesSerializationTag = "TotalBytes";
+        private readonly long m_TotalBytes;
+
+        public MemorySize(SerializationInfo serializationInfo, StreamingContext streamingContext)
+        {
+            m_TotalBytes = serializationInfo.GetInt64(TotaBytesSerializationTag);
+        }
+
+        public MemorySize(string formattedMemorySize)
+            : this(new FileSizeParser().Parse(formattedMemorySize))
+        {
+        }
+
+        public MemorySize(long totalBytes)
+        {
+            m_TotalBytes = totalBytes;
+        }
+
+        public long TotalBytes
+        {
+            get { return m_TotalBytes; }
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(TotaBytesSerializationTag, m_TotalBytes);
+        }
+    }
+
+    public class FileSizeContext
+    {
+        private string input;
+        private long output;
+
+        public FileSizeContext(string input)
+        {
+            Input = input;
+        }
+
+        public string Input { get; set; }
+
+        public long Output { get; set; }
+    }
+
+    public abstract class FileSizeExpression
+    {
+        public abstract void Interpret(FileSizeContext value);
+    }
+
+    public abstract class TerminalFileSizeExpression : FileSizeExpression
+    {
+        public override void Interpret(FileSizeContext value)
+        {
+            if (value.Input.EndsWith(ThisPattern()))
+            {
+                var amount = double.Parse(value.Input.Replace(ThisPattern(), string.Empty));
+                var fileSize = (long) (amount*1024);
+                value.Input = string.Format("{0}{1}", fileSize, NextPattern());
+                value.Output = fileSize;
+            }
+        }
+
+        protected abstract string ThisPattern();
+        protected abstract string NextPattern();
+    }
+
+    public class KbFileSizeExpression : TerminalFileSizeExpression
+    {
+        protected override string ThisPattern()
+        {
+            return "KB";
+        }
+
+        protected override string NextPattern()
+        {
+            return "bytes";
+        }
+    }
+
+    public class MbFileSizeExpression : TerminalFileSizeExpression
+    {
+        protected override string ThisPattern()
+        {
+            return "MB";
+        }
+
+        protected override string NextPattern()
+        {
+            return "KB";
+        }
+    }
+
+    public class GbFileSizeExpression : TerminalFileSizeExpression
+    {
+        protected override string ThisPattern()
+        {
+            return "GB";
+        }
+
+        protected override string NextPattern()
+        {
+            return "MB";
+        }
+    }
+
+    public class TbFileSizeExpression : TerminalFileSizeExpression
+    {
+        protected override string ThisPattern()
+        {
+            return "TB";
+        }
+
+        protected override string NextPattern()
+        {
+            return "GB";
+        }
+    }
+
+    public class FileSizeParser : FileSizeExpression
+    {
+        private readonly List<FileSizeExpression> expressionTree = new List<FileSizeExpression>
+        {
+            new TbFileSizeExpression(),
+            new GbFileSizeExpression(),
+            new MbFileSizeExpression(),
+            new KbFileSizeExpression()
+        };
+
+        public override void Interpret(FileSizeContext value)
+        {
+            foreach (var exp in expressionTree)
+            {
+                exp.Interpret(value);
+            }
+        }
+
+        public long Parse(string input)
+        {
+            var fileSizeContext = new FileSizeContext(input);
+            Interpret(fileSizeContext);
+            return fileSizeContext.Output;
+        }
+    }
+}
