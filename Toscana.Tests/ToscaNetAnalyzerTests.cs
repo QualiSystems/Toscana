8075016f4dc4b850ac57e045c7a76d5e64acb868
Boris Modylevsky <borismod@gmail.com>
2016-06-07 12:43:25 +0300

    Artifacts were added to NodeTemplate

--- Toscana.Tests/ToscaNetAnalyzerTests.cs
+++ Toscana.Tests/ToscaNetAnalyzerTests.cs
@@ -58,6 +58,189 @@ topology_template:
         }
 
         [Test]
+        public void Analyze_Overriding_Behavior_of_Predefined_Node_Types()
+        {
+            const string toscaString = @"tosca_definitions_version: tosca_simple_yaml_1_0
+ 
+description: Template for deploying a single server with MySQL software on top.
+ 
+topology_template:
+  inputs:
+    # omitted here for brevity
+ 
+  node_templates:
+    mysql:
+      type: tosca.nodes.DBMS.MySQL
+      properties:
+        root_password: { get_input: my_mysql_rootpw }
+        port: { get_input: my_mysql_port }
+      requirements:
+        - host: db_server
+      interfaces:
+        Standard:
+          configure: scripts/my_own_configure.sh
+ 
+    db_server:
+      type: tosca.nodes.Compute
+      capabilities:
+        # omitted here for brevity";
+
+            var tosca = new ToscaNetAnalyzer().Analyze(toscaString);
+
+            // Assert
+            tosca.ToscaDefinitionsVersion.Should().Be("tosca_simple_yaml_1_0");
+            tosca.Description.Should().Be("Template for deploying a single server with MySQL software on top.");
+            var topologyTemplate = tosca.TopologyTemplate;
+
+            topologyTemplate.Inputs.Should().BeNull();
+            topologyTemplate.Outputs.Should().BeNull();
+
+            topologyTemplate.NodeTemplates.Should().HaveCount(2);
+
+            var mysqlNodeTemplate = topologyTemplate.NodeTemplates["mysql"];
+            mysqlNodeTemplate.Type.Should().Be("tosca.nodes.DBMS.MySQL");
+            var requirementKeyValue = mysqlNodeTemplate.Requirements.Single().Single();
+            requirementKeyValue.Key.Should().Be("host");
+            requirementKeyValue.Value.Should().Be("db_server");
+            var standardInterface = (IDictionary<object, object>) mysqlNodeTemplate.Interfaces["Standard"];
+            standardInterface["configure"].Should().Be("scripts/my_own_configure.sh");
+
+            var dbServerNodeTemplate = topologyTemplate.NodeTemplates["db_server"];
+            dbServerNodeTemplate.Type.Should().Be("tosca.nodes.Compute");
+            dbServerNodeTemplate.Capabilities.Should().BeNull();
+            dbServerNodeTemplate.Requirements.Should().BeNull();
+        }
+
+        [Test]
+        public void Analyze_Template_For_a_Simple_Software_Installation()
+        {
+            const string toscaString = @"tosca_definitions_version: tosca_simple_yaml_1_0
+description: Template for deploying a single server with MySQL software on top.
+ 
+topology_template:
+  inputs:
+    # omitted here for brevity
+ 
+  node_templates:
+    mysql:
+      type: tosca.nodes.DBMS.MySQL
+      properties:
+        root_password: { get_input: my_mysql_rootpw }
+        port: { get_input: my_mysql_port }
+      requirements:
+        - host: db_server
+ 
+    db_server:
+      type: tosca.nodes.Compute
+      capabilities:
+        # omitted here for brevity";
+
+            var tosca = new ToscaNetAnalyzer().Analyze(toscaString);
+
+            // Assert
+            tosca.ToscaDefinitionsVersion.Should().Be("tosca_simple_yaml_1_0");
+            tosca.Description.Should().Be("Template for deploying a single server with MySQL software on top.");
+            var topologyTemplate = tosca.TopologyTemplate;
+
+            topologyTemplate.Inputs.Should().BeNull();
+            topologyTemplate.Outputs.Should().BeNull();
+
+            topologyTemplate.NodeTemplates.Should().HaveCount(2);
+
+            var mysqlNodeTemplate = topologyTemplate.NodeTemplates["mysql"];
+            mysqlNodeTemplate.Type.Should().Be("tosca.nodes.DBMS.MySQL");
+            var requirementKeyValue = mysqlNodeTemplate.Requirements.Single().Single();
+            requirementKeyValue.Key.Should().Be("host");
+            requirementKeyValue.Value.Should().Be("db_server");
+
+            var dbServerNodeTemplate = topologyTemplate.NodeTemplates["db_server"];
+            dbServerNodeTemplate.Type.Should().Be("tosca.nodes.Compute");
+            dbServerNodeTemplate.Capabilities.Should().BeNull();
+            dbServerNodeTemplate.Requirements.Should().BeNull();
+        }
+
+        [Test]
+        public void Analyze_Template_For_Database_Content_Deployment()
+        {
+            const string toscaString = @"tosca_definitions_version: tosca_simple_yaml_1_0
+ 
+description: Template for deploying MySQL and database content.
+ 
+topology_template:
+  inputs:
+    # omitted here for brevity
+ 
+  node_templates:
+    my_db:
+      type: tosca.nodes.Database.MySQL
+      properties:
+        name: { get_input: database_name }
+        user: { get_input: database_user }
+        password: { get_input: database_password }
+        port: { get_input: database_port }
+      artifacts:
+        db_content:
+          file: files/my_db_content.txt
+          type: tosca.artifacts.File
+      requirements:
+        - host: mysql
+      interfaces:
+        Standard:
+          create:
+            implementation: db_create.sh
+            inputs:
+              # Copy DB file artifact to server’s staging area
+              db_data: { get_artifact: [ SELF, db_content ] }
+ 
+    mysql:
+      type: tosca.nodes.DBMS.MySQL
+      properties:
+        root_password: { get_input: mysql_rootpw }
+        port: { get_input: mysql_port }
+      requirements:
+        - host: db_server
+ 
+    db_server:
+      type: tosca.nodes.Compute
+      capabilities:
+        # omitted here for brevity";
+
+            var tosca = new ToscaNetAnalyzer().Analyze(toscaString);
+
+            // Assert
+            tosca.ToscaDefinitionsVersion.Should().Be("tosca_simple_yaml_1_0");
+            tosca.Description.Should().Be("Template for deploying MySQL and database content.");
+            var topologyTemplate = tosca.TopologyTemplate;
+
+            topologyTemplate.Inputs.Should().BeNull();
+            topologyTemplate.Outputs.Should().BeNull();
+
+            topologyTemplate.NodeTemplates.Should().HaveCount(3);
+
+            var mysqlNodeTemplate = topologyTemplate.NodeTemplates["mysql"];
+            mysqlNodeTemplate.Type.Should().Be("tosca.nodes.DBMS.MySQL");
+            var requirementKeyValue = mysqlNodeTemplate.Requirements.Single().Single();
+            requirementKeyValue.Key.Should().Be("host");
+            requirementKeyValue.Value.Should().Be("db_server");
+
+            var dbServerNodeTemplate = topologyTemplate.NodeTemplates["db_server"];
+            dbServerNodeTemplate.Type.Should().Be("tosca.nodes.Compute");
+            dbServerNodeTemplate.Capabilities.Should().BeNull();
+            dbServerNodeTemplate.Requirements.Should().BeNull();
+
+            var myDbNodeTemplate = topologyTemplate.NodeTemplates["my_db"];
+            myDbNodeTemplate.Type.Should().Be("tosca.nodes.Database.MySQL");
+            myDbNodeTemplate.Capabilities.Should().BeNull();
+            myDbNodeTemplate.Requirements.Single()["host"].Should().Be("mysql");
+            myDbNodeTemplate.Properties.Should().HaveCount(4);
+
+            myDbNodeTemplate.Artifacts.Should().HaveCount(1);
+            var artifact = myDbNodeTemplate.Artifacts["db_content"];
+            artifact.File.Should().Be("files/my_db_content.txt");
+            artifact.Type.Should().Be("tosca.artifacts.File");
+        }
+
+        [Test]
         public void Analyze_With_Inputs_AllDataAnalyzed()
         {
             const string toscaString = @"tosca_definitions_version: tosca_simple_yaml_1_0
@@ -115,107 +298,6 @@ topology_template:
             ((IDictionary<object, object>) hostProperties.NumCpus)["get_input"].Should().Be("cpus");
             hostProperties.MemSize.Should().Be(new DigitalStorage("2 GB"));
             hostProperties.DiskSize.Should().Be(new DigitalStorage("10 GB"));
-        }
-
-        [Test]
-        public void Analyze_Template_For_a_Simple_Software_Installation()
-        {
-            const string toscaString = @"tosca_definitions_version: tosca_simple_yaml_1_0
-description: Template for deploying a single server with MySQL software on top.
- 
-topology_template:
-  inputs:
-    # omitted here for brevity
- 
-  node_templates:
-    mysql:
-      type: tosca.nodes.DBMS.MySQL
-      properties:
-        root_password: { get_input: my_mysql_rootpw }
-        port: { get_input: my_mysql_port }
-      requirements:
-        - host: db_server
- 
-    db_server:
-      type: tosca.nodes.Compute
-      capabilities:
-        # omitted here for brevity";
-
-            var tosca = new ToscaNetAnalyzer().Analyze(toscaString);
-
-            // Assert
-            tosca.ToscaDefinitionsVersion.Should().Be("tosca_simple_yaml_1_0");
-            tosca.Description.Should().Be("Template for deploying a single server with MySQL software on top.");
-            var topologyTemplate = tosca.TopologyTemplate;
-
-            topologyTemplate.Inputs.Should().BeNull();
-            topologyTemplate.Outputs.Should().BeNull();
-
-            topologyTemplate.NodeTemplates.Should().HaveCount(2);
-
-            var mysqlNodeTemplate = topologyTemplate.NodeTemplates["mysql"];
-            mysqlNodeTemplate.Type.Should().Be("tosca.nodes.DBMS.MySQL");
-            var requirementKeyValue = mysqlNodeTemplate.Requirements.Single().Single();
-            requirementKeyValue.Key.Should().Be("host");
-            requirementKeyValue.Value.Should().Be("db_server");
-
-            var dbServerNodeTemplate = topologyTemplate.NodeTemplates["db_server"];
-            dbServerNodeTemplate.Type.Should().Be("tosca.nodes.Compute");
-            dbServerNodeTemplate.Capabilities.Should().BeNull();
-            dbServerNodeTemplate.Requirements.Should().BeNull();
-        }
-
-        [Test]
-        public void Analyze_Overriding_Behavior_of_Predefined_Node_Types()
-        {
-            const string toscaString = @"tosca_definitions_version: tosca_simple_yaml_1_0
- 
-description: Template for deploying a single server with MySQL software on top.
- 
-topology_template:
-  inputs:
-    # omitted here for brevity
- 
-  node_templates:
-    mysql:
-      type: tosca.nodes.DBMS.MySQL
-      properties:
-        root_password: { get_input: my_mysql_rootpw }
-        port: { get_input: my_mysql_port }
-      requirements:
-        - host: db_server
-      interfaces:
-        Standard:
-          configure: scripts/my_own_configure.sh
- 
-    db_server:
-      type: tosca.nodes.Compute
-      capabilities:
-        # omitted here for brevity";
-
-            var tosca = new ToscaNetAnalyzer().Analyze(toscaString);
-
-            // Assert
-            tosca.ToscaDefinitionsVersion.Should().Be("tosca_simple_yaml_1_0");
-            tosca.Description.Should().Be("Template for deploying a single server with MySQL software on top.");
-            var topologyTemplate = tosca.TopologyTemplate;
-
-            topologyTemplate.Inputs.Should().BeNull();
-            topologyTemplate.Outputs.Should().BeNull();
-
-            topologyTemplate.NodeTemplates.Should().HaveCount(2);
-
-            var mysqlNodeTemplate = topologyTemplate.NodeTemplates["mysql"];
-            mysqlNodeTemplate.Type.Should().Be("tosca.nodes.DBMS.MySQL");
-            var requirementKeyValue = mysqlNodeTemplate.Requirements.Single().Single();
-            requirementKeyValue.Key.Should().Be("host");
-            requirementKeyValue.Value.Should().Be("db_server");
-            mysqlNodeTemplate.Interfaces["Standard"]["configure"].Should().Be("scripts/my_own_configure.sh");
-
-            var dbServerNodeTemplate = topologyTemplate.NodeTemplates["db_server"];
-            dbServerNodeTemplate.Type.Should().Be("tosca.nodes.Compute");
-            dbServerNodeTemplate.Capabilities.Should().BeNull();
-            dbServerNodeTemplate.Requirements.Should().BeNull();
         }
     }
 }
--- Toscana/Domain/NodeTemplate.cs
+++ Toscana/Domain/NodeTemplate.cs
@@ -10,7 +10,14 @@ namespace Toscana.Domain
         public ComputeCapabilities Capabilities { get; set; }
         public Dictionary<string, object> Properties { get; set; }
         public List<Dictionary<string, object>> Requirements { get; set; }
-        public Dictionary<string, Dictionary<string, string>> Interfaces { get; set; }
+        public Dictionary<string, object> Interfaces { get; set; }
+        public Dictionary<string, Artifact> Artifacts { get; set; }
+    }
+
+    public class Artifact
+    {
+        public string Type { get; set; }
+        public string File { get; set; }
     }
 
     public class ComputeCapabilities
