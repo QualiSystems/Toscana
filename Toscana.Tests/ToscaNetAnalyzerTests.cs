f026d5643237514ed7940db16cdda1cf45e68824
Boris Modylevsky <borismod@gmail.com>
2016-06-08 13:35:09 +0300

    Analyze_Template_For_A_Two_Tier_Application was added

--- Toscana.Tests/ToscaNetAnalyzerTests.cs
+++ Toscana.Tests/ToscaNetAnalyzerTests.cs
@@ -241,6 +241,177 @@ topology_template:
         }
 
         [Test]
+        public void Analyze_Template_For_A_Two_Tier_Application()
+        {
+            const string toscaString = @"tosca_definitions_version: tosca_simple_yaml_1_0
+ 
+description: Template for deploying a two-tier application servers on two
+ 
+topology_template:
+  inputs:
+    # Admin user name and password to use with the WordPress application
+    wp_admin_username:
+      type: string
+    wp_admin_password:
+      type: string
+    wp_db_name:
+      type: string
+    wp_db_user:
+      type: string
+    wp_db_password:
+      type: string
+    wp_db_port:
+      type: integer
+    mysql_root_password:
+      type: string
+    mysql_port:
+      type: integer
+    context_root:
+      type: string
+ 
+  node_templates:
+    wordpress:
+      type: tosca.nodes.WebApplication.WordPress
+      properties:
+        context_root: { get_input: context_root }
+        admin_user: { get_input: wp_admin_username }
+        admin_password: { get_input: wp_admin_password }
+        db_host: { get_attribute: [ db_server, private_address ] }
+      requirements:
+        - host: apache
+        - database_endpoint: wordpress_db
+      interfaces:
+        Standard:
+          inputs:
+            db_host: { get_attribute: [ db_server, private_address ] }
+            db_port: { get_property: [ wordpress_db, port ] }
+            db_name: { get_property: [ wordpress_db, name ] }
+            db_user: { get_property: [ wordpress_db, user ] }
+            db_password: { get_property: [ wordpress_db, password ] }  
+ 
+    apache:
+      type: tosca.nodes.WebServer.Apache
+      properties:
+        # omitted here for brevity
+      requirements:
+        - host: web_server
+ 
+    web_server:
+      type: tosca.nodes.Compute
+      capabilities:
+        # omitted here for brevity
+ 
+    wordpress_db:
+      type: tosca.nodes.Database.MySQL
+      properties:
+        name: { get_input: wp_db_name }
+        user: { get_input: wp_db_user }
+        password: { get_input: wp_db_password }
+        port: { get_input: wp_db_port }
+      requirements:
+        - host: mysql
+ 
+    mysql:
+      type: tosca.nodes.DBMS.MySQL
+      properties:
+        root_password: { get_input: mysql_root_password }
+        port: { get_input: mysql_port }
+      requirements:
+        - host: db_server
+ 
+    db_server:
+      type: tosca.nodes.Compute
+      capabilities:
+        # omitted here for brevity";
+
+            var tosca = new ToscaNetAnalyzer().Analyze(toscaString);
+
+            // Assert
+            tosca.ToscaDefinitionsVersion.Should().Be("tosca_simple_yaml_1_0");
+            tosca.Description.Should().Be("Template for deploying a two-tier application servers on two");
+            var topologyTemplate = tosca.TopologyTemplate;
+
+            #region Topology Template Input & Outputs
+
+            topologyTemplate.Inputs["wp_admin_username"].Type.Should().Be("string");
+            topologyTemplate.Inputs["wp_admin_password"].Type.Should().Be("string");
+            topologyTemplate.Inputs["wp_db_name"].Type.Should().Be("string");
+            topologyTemplate.Inputs["wp_db_password"].Type.Should().Be("string");
+            topologyTemplate.Inputs["wp_db_port"].Type.Should().Be("integer");
+            topologyTemplate.Inputs["mysql_root_password"].Type.Should().Be("string");
+            topologyTemplate.Inputs["mysql_port"].Type.Should().Be("integer");
+            topologyTemplate.Inputs["context_root"].Type.Should().Be("string");
+            topologyTemplate.Outputs.Should().BeNull();
+
+            #endregion
+
+            topologyTemplate.NodeTemplates.Should().HaveCount(6);
+
+            #region wordpress
+
+            var wordpressNodeTemplate = topologyTemplate.NodeTemplates["wordpress"];
+            wordpressNodeTemplate.Type.Should().Be("tosca.nodes.WebApplication.WordPress");
+            wordpressNodeTemplate.Capabilities.Should().BeNull();
+            wordpressNodeTemplate.Requirements.Should().HaveCount(2);
+            wordpressNodeTemplate.Requirements.First()["host"].Should().Be("apache");
+            wordpressNodeTemplate.Requirements.Last()["database_endpoint"].Should().Be("wordpress_db");
+            wordpressNodeTemplate.Properties.Should().HaveCount(4);
+
+            #endregion
+
+            #region apache
+
+            var apacheNodeTemplate = topologyTemplate.NodeTemplates["apache"];
+            apacheNodeTemplate.Type.Should().Be("tosca.nodes.WebServer.Apache");
+            apacheNodeTemplate.Capabilities.Should().BeNull();
+            apacheNodeTemplate.Requirements.Should().HaveCount(1);
+            apacheNodeTemplate.Requirements.Single()["host"].Should().Be("web_server");
+            apacheNodeTemplate.Properties.Should().BeNull();
+
+            #endregion
+
+            #region web_server
+
+            var webServerNodeTemplate = topologyTemplate.NodeTemplates["web_server"];
+            webServerNodeTemplate.Type.Should().Be("tosca.nodes.Compute");
+            webServerNodeTemplate.Capabilities.Should().BeNull();
+            webServerNodeTemplate.Requirements.Should().BeNull();
+            webServerNodeTemplate.Properties.Should().BeNull();
+
+            #endregion
+
+            #region wordpress_db
+
+            var wordpressDbNodeTemplate = topologyTemplate.NodeTemplates["wordpress_db"];
+            wordpressDbNodeTemplate.Type.Should().Be("tosca.nodes.Database.MySQL");
+            wordpressDbNodeTemplate.Capabilities.Should().BeNull();
+            wordpressDbNodeTemplate.Requirements.Should().HaveCount(1);
+            wordpressDbNodeTemplate.Requirements.Single()["host"].Should().Be("mysql");
+            wordpressDbNodeTemplate.Properties.Should().HaveCount(4);
+
+            #endregion
+
+            #region mysql
+
+            var mysqlNodeTemplate = topologyTemplate.NodeTemplates["mysql"];
+            mysqlNodeTemplate.Type.Should().Be("tosca.nodes.DBMS.MySQL");
+            var requirementKeyValue = mysqlNodeTemplate.Requirements.Single().Single();
+            requirementKeyValue.Key.Should().Be("host");
+            requirementKeyValue.Value.Should().Be("db_server");
+
+            #endregion
+
+            #region db_server
+
+            var dbServerNodeTemplate = topologyTemplate.NodeTemplates["db_server"];
+            dbServerNodeTemplate.Type.Should().Be("tosca.nodes.Compute");
+            dbServerNodeTemplate.Capabilities.Should().BeNull();
+            dbServerNodeTemplate.Requirements.Should().BeNull();
+
+            #endregion
+        }
+
+        [Test]
         public void Analyze_With_Inputs_AllDataAnalyzed()
         {
             const string toscaString = @"tosca_definitions_version: tosca_simple_yaml_1_0
