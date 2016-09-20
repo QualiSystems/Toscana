cb46469b60c069031afdc2c9fb670d130c0aaa5c
johnathanvidu <johnathan.vidu@gmail.com>
2016-09-20 17:52:39 +0300

    Fixed bug 156457. Two or more node types refering to the same artifact caused key already exists exception

--- Toscana.Tests/Engine/ToscaCloudServiceArchiveLoaderTests.cs
+++ Toscana.Tests/Engine/ToscaCloudServiceArchiveLoaderTests.cs
@@ -698,5 +698,52 @@ node_types:
 
             action.ShouldNotThrow("It should be possible to import the same file from different files");
         }
+        
+        [Test]
+        public void It_Is_Possible_To_Have_Two_Different_Nodes_Refering_To_Same_Artifact()
+        {
+            // Arrange
+            var toscaMetaContent = @"
+TOSCA-Meta-File-Version: 1.0
+CSAR-Version: 1.1
+Created-By: Vido
+Entry-Definitions: entry.yaml";
+
+            var toscaEntryContent = @"
+tosca_definitions_version: tosca_simple_yaml_1_0
+
+node_types:
+  example.TransactionSubsystem:
+    properties:
+      vendor:
+        type: string
+    artifacts:
+      icon:
+        file: sample.png
+        type: tosca.artifacts.File
+
+  example.CrapsactionSubsystem:
+    properties:
+      vendor:
+        type: string
+    artifacts:
+      icon:
+        file: sample.png
+        type: tosca.artifacts.File
+";
+
+            var fileContents = new List<FileContent>
+            {
+                new FileContent(@"TOSCA-Metadata/TOSCA.meta", toscaMetaContent),
+                new FileContent("entry.yaml", toscaEntryContent),
+                new FileContent("sample.png", "")
+            };
+
+            fileSystem.CreateArchive("tosca.zip", fileContents);
+
+            Action action = () => toscaCloudServiceArchiveLoader.Load("tosca.zip");
+
+            action.ShouldNotThrow("It should be possible to that two node types will reference the same artifact");
+        }
     }
 }
