0f4821df3ee220e052a351853bab0c819bd94f2a
Boris Modylevsky <borismod@gmail.com>
2016-09-25 09:53:12 +0300

    mend

---
+++ Toscana.Tests/Engine/ToscaServiceTemplateSaverTests.cs
@@ -0,0 +1,29 @@
+﻿using System.IO.Abstractions.TestingHelpers;
+using FluentAssertions;
+using NUnit.Framework;
+using Toscana.Engine;
+
+namespace Toscana.Tests.Engine
+{
+    [TestFixture]
+    public class ToscaServiceTemplateSaverTests
+    {
+        [Test]
+        public void It_Is_Possible_To_Save_Service_Template_To_File()
+        {
+            var mockFileSystem = new MockFileSystem();
+            var bootstrapper = new Bootstrapper();
+            var serviceTemplateSaver = new ToscaServiceTemplateSaver(mockFileSystem,
+                bootstrapper.GetToscaServiceTemplateSerializer());
+
+            var serviceTemplate = new ToscaServiceTemplate
+            {
+                ToscaDefinitionsVersion = "tosca_simple_yaml_1_0"
+            };
+
+            serviceTemplateSaver.Save(@"c:\files\service_template.yml", serviceTemplate);
+
+            mockFileSystem.FileExists(@"c:\files\service_template.yml").Should().BeTrue();
+        }
+    }
+}
