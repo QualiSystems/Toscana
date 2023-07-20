9c51fd9104706c1fd745ebd5ab0781c300b8ea7c
DxCK <DxCK@users.noreply.github.com>
2023-07-20 11:45:59 +0300

    reverted unneeded modifications

--- Toscana.Tests/CustomerTests.cs
+++ Toscana.Tests/CustomerTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 using System.IO;
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
@@ -17,7 +16,7 @@ namespace Toscana.Tests
         {
             var mockFileSystem = new MockFileSystem();
             DependencyResolver.Current.Replace<IFileSystem>(mockFileSystem);
-            
+
             mockFileSystem.AddFile(Path.Combine(Environment.CurrentDirectory, "standard.yaml"),
                 new MockFileData(@"
 tosca_definitions_version: tosca_simple_yaml_1_0"));
